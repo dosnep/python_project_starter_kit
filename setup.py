29f0cc898f68e4d2114d016b60fe865f34a62da8
pierre.dosne <pierre.dosne@thalesgroup.com>
2020-09-29 22:17:29 +0200

    first version

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name="project",
+    version="0.0.1",
+    keywords=["project"],
+    install_requires=[
+    ],    
+    packages=setuptools.find_packages(),
+)
+
